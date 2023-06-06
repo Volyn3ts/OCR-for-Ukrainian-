c21caf30e4cf7f0371a668a71bc2a9a9a9a7512c
Volyn3ts <volynetskolia@gmail.com>
2023-06-06 17:38:01 +0300

    Added files

--- ocr.py
+++ ocr.py
@@ -1,6 +1,4 @@
 import pytesseract
-import cv2
-import argparse
 from gooey import Gooey, GooeyParser
 import os
 from pdf2image import convert_from_path
@@ -12,7 +10,7 @@ path = os.getcwd()
 full_path_tessdata = os.path.join(path, r'TESSERACT\tessdata')
 full_path_tesseract = os.path.join(path, r'TESSERACT\tesseract.exe')
 
-TESSDATA_PREFIX=full_path_tessdata
+TESSDATA_PREFIX = full_path_tessdata
 pytesseract.pytesseract.tesseract_cmd = full_path_tesseract
 
 
@@ -25,14 +23,15 @@ pytesseract.pytesseract.tesseract_cmd = full_path_tesseract
 def start_interface():
     ap = GooeyParser()
     ap.add_argument('Image', help="image to process", widget='FileChooser')
-    ap.add_argument("-p", "--PSM", type=int, default=3, help="Tesseract PSM mode,\n3-auto page segmentation,\n13-single raw line")
+    ap.add_argument("-p", "--PSM", type=int, default=3,
+                    help="Tesseract PSM mode,\n3-auto page segmentation,\n13-single raw line")
     args = vars(ap.parse_args())
     print('Arguments parsed')
     mydoc = docx.Document()
     if '.pdf' in args["Image"]:
         images = convert_from_path(args["Image"], poppler_path=os.path.join(path, r'bin\poppler\Library\bin'))
         for i in range(len(images)):
-            images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+            images[i].save('page' + str(i) + '.jpg', 'JPEG')
             image = Image.open(rf'page{i}.jpg')
             image = image.convert('1', dither=Image.NONE)
             print('Reading image')
@@ -40,7 +39,7 @@ def start_interface():
             print("OCR'ing")
             text = pytesseract.image_to_string(image, config=options)
             paragraph = mydoc.add_paragraph(text)
-            paragraph.alignment = 0 # 0-left, 1-center, 2-right
+            paragraph.alignment = 0  # 0-left, 1-center, 2-right
     else:
         image = Image.open(args["Image"])
         image = image.convert('1', dither=Image.NONE)
@@ -49,12 +48,11 @@ def start_interface():
         print("OCR'ing")
         text = pytesseract.image_to_string(image, config=options)
         paragraph = mydoc.add_paragraph(text)
-        paragraph.alignment = 0 # 0-left, 1-center, 2-right
+        paragraph.alignment = 0  # 0-left, 1-center, 2-right
     mydoc.save(os.path.join(path, fr'{ntpath.basename(args["Image"])}.docx'))
     print("Finished, result in 'result.txt'")
     os.startfile(os.path.join(path, fr'{ntpath.basename(args["Image"])}.docx'))
 
-    
 
 if __name__ == '__main__':
     start_interface()
